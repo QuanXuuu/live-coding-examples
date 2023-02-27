98a62a35de83c6c5cebcf4543133314feaccb601
Quan Xu <xuquan69220@gmail.com>
2023-02-27 10:52:22 +0100

    Add comments file

---
+++ 3_pb/1_language/1_basic-demonstrations/0_comments.js
@@ -0,0 +1,36 @@
+// Einzeilige kommentare
+
+// Dies ist eine zeile
+// naechste zeile
+// noch eine zeile
+
+// Ein einzeiles kommentar, geht immer nur ueber eine zeile.
+
+const bla = 5;
+
+const bla2 = 10; // bla2 sollte man umbennenen
+
+/* Dies ist
+   ein
+   mehrzeiliger kommentar*/
+
+// JSDoc style kommentare
+
+/**
+ * Dies ist eine JSDoc
+ * Kommentar...
+ * 
+ */
+
+/**
+ * @function testFunktion
+ * @see https://jsdoc.app/
+ * @description Dies ist eine testfunktion
+ * @param {string} testWert1 ist ein testparameter
+ * @returns {string}
+ */
+
+function testFunction(testWert1)
+{
+    
+}
